cdc92981f6de93e2a90fd76cc700a4792c241b29
Tomoya KASHIMADA <tomoya@sforzando.co.jp>
2020-04-15 00:46:52 +0900

    :+1: Add sample code with alpine.js

---
+++ static/js/main.js
@@ -0,0 +1,8 @@
+function camera() {
+    return {
+        show: false,
+        open() { this.show = true },
+        close() { this.show = false },
+        isOpen() { return this.show === true },
+    }
+}
