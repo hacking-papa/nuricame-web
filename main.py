024e41506277ae15edc4840e688f0a741ae99e31
Shin'ichiro SUZUKI <shin@sforzando.co.jp>
2020-04-15 07:13:26 +0900

    :+1: Add opencv-python

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 import os
+import time
 
-from flask import Flask, render_template
+import cv2
+import numpy as np
+from flask import Flask, render_template, request, make_response
 
 if os.getenv("GAE_ENV", "").startswith("standard"):
     # Production in the standard environment
@@ -18,8 +21,23 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    app.logger.info("GET /index")
-    return render_template("index.html", title="ぬりカメ")
+    if request.method == "GET":
+        app.logger.info("GET /index")
+        return render_template("index.html", title="ぬりカメ")
+    elif request.method == "POST":
+        app.logger.info("POST /index")
+        start = time.time()
+        image = request.files.get("image")
+        app.logger.debug(f"Uploaded: {image}")
+        img = np.frombuffer(image.read(), dtype=np.uint8)
+        img = cv2.imdecode(img, 1)
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+        data = cv2.imencode(".jpg", img)[1].tostring()
+        response = make_response()
+        response.data = data
+        response.mimetype = "image/jpeg"
+        app.logger.debug(f"Elapsed Time: {time.time() - start}")
+        return response
 
 
 if __name__ == "__main__":
