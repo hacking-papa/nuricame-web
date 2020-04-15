4c2c21e95da7e6423580c84b773af858570c67b6
Shin'ichiro SUZUKI <shin@sforzando.co.jp>
2020-04-16 05:16:32 +0900

    :wrench: Rewrite index.html

--- static/js/main.js
+++ static/js/main.js
@@ -1,14 +1,39 @@
-function camera() {
+function takePhoto() {
   return {
     show: false,
     open() {
+      console.log("takePhoto.open()");
       this.show = true;
     },
     close() {
+      console.log("takePhoto.close()");
       this.show = false;
     },
     isOpen() {
-      return this.show === true;
+      console.log("takePhoto.isOpen()");
+      return this.show;
+    },
+  };
+}
+
+function selectPhoto() {
+  return {
+    show: false,
+    open() {
+      console.log("selectPhoto.open()");
+      this.show = true;
+    },
+    close() {
+      console.log("selectPhoto.close()");
+      this.show = false;
+    },
+    isOpen() {
+      console.log("selectPhoto.isOpen()");
+      return this.show;
+    },
+    post() {
+      console.log("selectPhoto.post()");
+      // TODO: 選択された画像をPOST
     },
   };
 }
